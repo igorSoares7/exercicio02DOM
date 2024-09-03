e0bc1f2a001147e83cc4ffa7f38e154fd7c27236
igorSoares7 <igorsoaresr_contato@hotmail.com>
2024-09-03 20:08:06 -0300

    Corrigie o for da função imprimePares

--- script04.js
+++ script04.js
@@ -1,7 +1,7 @@
 function imprimePares() {
     let imprimeResultado = document.getElementById("numerosPar")
     let pares = ''
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 2 == 0) {
             pares += `${i} | `
         }
